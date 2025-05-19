1287088ff1b5a3d402f49ef7b5b55440844af777
Raphael Eduardo <raphaeleduardosc@gmail.com>
2025-05-19 18:45:12 -0300

    Entidades

---
+++ desafio-02/src/main/java/com/raphael/evento/Desafio02Application.java
@@ -0,0 +1,13 @@
+package com.raphael.evento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio02Application.class, args);
+	}
+
+}
---
+++ desafio-02/src/main/java/com/raphael/evento/entities/Atividade.java
@@ -0,0 +1,62 @@
+package com.raphael.evento.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private String descricao;
+	private Double preco;
+
+	public Atividade() {
+	}
+
+	public Atividade(Integer id, String name, String descricao, Double preco) {
+		this.id = id;
+		this.name = name;
+		this.descricao = descricao;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+}
