fd1eb08df2dd210c05d1ba442624e95b1029bd0c
Raphael Eduardo <raphaeleduardosc@gmail.com>
2025-05-20 18:58:46 -0300

    GET by ID

--- desafio-03/src/main/java/com/raphael/crud/controllers/ClientController.java
+++ desafio-03/src/main/java/com/raphael/crud/controllers/ClientController.java
@@ -1,13 +1,26 @@
 package com.raphael.crud.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.raphael.crud.dto.ClientDTO;
 import com.raphael.crud.services.ClientService;
 
+@RestController
+@RequestMapping(value = "/clients")
 public class ClientController {
 
 	@Autowired
 	private ClientService service;
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+		ClientDTO dto = service.findById(id);
+		return ResponseEntity.ok(dto);
+	}
 	
 }
--- desafio-03/src/main/java/com/raphael/crud/services/ClientService.java
+++ desafio-03/src/main/java/com/raphael/crud/services/ClientService.java
@@ -2,13 +2,12 @@ package com.raphael.crud.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.raphael.crud.dto.ClientDTO;
 import com.raphael.crud.entities.Client;
 import com.raphael.crud.repositories.ClientRepository;
 import com.raphael.crud.services.exceptions.ResourceNotFoundException;
-
-import jakarta.transaction.Transactional;
 
 @Service
 public class ClientService {
@@ -16,7 +15,7 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	@Transactional
+	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Client c = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado"));
 		return new ClientDTO(c);
