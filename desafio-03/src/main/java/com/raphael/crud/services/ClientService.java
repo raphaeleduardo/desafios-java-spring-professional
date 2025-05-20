f078319eff084624fedeae9532958d7c9513fbd8
Raphael Eduardo <raphaeleduardosc@gmail.com>
2025-05-20 19:04:38 -0300

    GET All

--- desafio-03/src/main/java/com/raphael/crud/services/ClientService.java
+++ desafio-03/src/main/java/com/raphael/crud/services/ClientService.java
@@ -1,6 +1,8 @@
 package com.raphael.crud.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,4 +22,12 @@ public class ClientService {
 		Client c = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado"));
 		return new ClientDTO(c);
 	}
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAll(Pageable pageable) {
+		Page<Client> result = repository.findAll(pageable);
+		return result.map(x -> new ClientDTO(x));
+	}
+	
+	
 }
