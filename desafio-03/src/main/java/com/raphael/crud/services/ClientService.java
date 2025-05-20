39a41c8c3dcfb1a942abdf2a21076af16fd6441f
Raphael Eduardo <raphaeleduardosc@gmail.com>
2025-05-20 18:53:09 -0300

    exception

--- desafio-03/src/main/java/com/raphael/crud/services/ClientService.java
+++ desafio-03/src/main/java/com/raphael/crud/services/ClientService.java
@@ -3,7 +3,12 @@ package com.raphael.crud.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.raphael.crud.dto.ClientDTO;
+import com.raphael.crud.entities.Client;
 import com.raphael.crud.repositories.ClientRepository;
+import com.raphael.crud.services.exceptions.ResourceNotFoundException;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ClientService {
@@ -11,5 +16,9 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	
+	@Transactional
+	public ClientDTO findById(Long id) {
+		Client c = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado"));
+		return new ClientDTO(c);
+	}
 }
